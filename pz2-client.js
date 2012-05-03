bf54290b35783ab43cbc1d39a2a4cb6c04d868a5
ssp <porst@sub.uni-goettingen.de>
2012-05-03 17:16:18 +0200

    Add German and English names for Source Types
    Source Types come from Guide data.

--- pz2-client.js
+++ pz2-client.js
@@ -5851,6 +5851,56 @@ var mediaNames = {
 
 
 
+/* Localised Source Types (for faceting Guide Records)
+*/
+var sourceTypeNames = {
+	'de': {
+		'sf': 'Hochschul- und Forschungsinstitute',
+		'so1': 'Fachgesellschaften',
+		'so2': 'Landesdienste',
+		'sc': 'Kommerzielle Anbieter',
+		'per': 'Personen',
+		'ref': 'Nachschlagewerke',
+		'dat': 'Daten und Quellen',
+		'man': 'Anleitungen, Lehrmaterialien',
+		'fp': 'Forschungsprojekte',
+		'kn': 'Kongresse',
+		'web': 'Webverzeichnisse',
+		'bl': 'Bibliographien',
+		'lib': 'Bibliotheken, Archive, Kataloge',
+		'img': 'Bildersammlungen, Filme',
+		'mus': 'Museen, Ausstellungen',
+		'do': 'Software',
+		'ka': 'Karten und Atlanten',
+		'vc': 'Listen',
+		'art': 'Zeitschriften und Artikel'
+	},
+
+	'en': {
+		'sf': 'Academic and Research Institutions',
+		'so1': 'Scholarly Organizations',
+		'so2': 'Official Governmental Servers',
+		'sc': 'Commercial Providers',
+		'per': 'Persons',
+		'ref': 'Reference Works',
+		'dat': 'Data and Sources',
+		'man': 'Manuals, Teaching Materials',
+		'fp': 'Current Research Projects',
+		'kn': 'Congresses',
+		'web': 'Web Directories',
+		'bl': 'Bibliographies',
+		'lib': 'Libraries, Archives, Catalogs',
+		'img': 'Collections of pictures, videos',
+		'mus': 'Museums, Exhibitions',
+		'do': 'Software ',
+		'ka': 'Maps and Atlases',
+		'vc': 'Lists',
+		'art': 'Journals and Articles'
+	}
+};
+
+
+
 /*	Localised Catalogue names
 */
 var catalogueNames = {
