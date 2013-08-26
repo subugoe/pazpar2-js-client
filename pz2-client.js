ceef19fe892e98fb90484e2de5c57a22b838f5ab
Sven-S. Porst <ssp-web@earthlingsoft.net>
2013-08-26 11:22:06 +0200

    fix seconds vs milliseconds error

--- pz2-client.js
+++ pz2-client.js
@@ -151,9 +151,9 @@ function initialisePazpar2 () {
 		pz2InitTimeout = undefined;
 	}
 
-	// Only run init if the previous init is more than 15s ago.
+	// Only run init if the previous init is more than 15 seconds ago.
 	var currentTime = (new Date).getTime();
-	if (pz2InitRequestStartTime + 15 < currentTime) {
+	if (pz2InitRequestStartTime + 15000 < currentTime) {
 		pz2InitRequestStartTime = currentTime;
 		my_paz.init(undefined, my_paz.serviceId);
 	}
